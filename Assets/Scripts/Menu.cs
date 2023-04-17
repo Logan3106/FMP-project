8b06899b378a7da9c2e0cef41121a2e7a69be59e
Logan Anta Fernandez <529295@escg.ac.uk>
2023-04-17 11:26:20 +0100

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    void Start()
+    {
+        startButton.Select();
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+        
+    }
+}
